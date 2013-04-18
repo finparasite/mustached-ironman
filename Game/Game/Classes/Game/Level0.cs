8a00c1e53876aae5b94f433c0f040187028dce49
Despite <despite.mn@gmail.com>
2013-04-19 00:30:03 +0300

    Debug code

--- Game/Game/Classes/Game/Level0.cs
+++ Game/Game/Classes/Game/Level0.cs
@@ -4,19 +4,24 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Game
 {
     class Level0 : Level
     {
+        private static Texture2D niki;
+
         public override void LoadContent(ContentManager Content)
         {
             // TODO
+            niki = Content.Load<Texture2D>(@"Bitmaps/niki");
         }
 
         public override void SetupLevel()
         {
             // TODO
+            player = new Player(Vector2.Zero, niki);
         }
     }
 }
