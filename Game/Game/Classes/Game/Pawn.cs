ff65035664000c87f579d8856aecb1632930b9f1
finparasite <finparasite@gmail.com>
2013-04-19 00:31:56 +0300

    Pawn

---
+++ Game/Game/Classes/Game/Pawn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Game
+{
+    class Pawn : DynamicActor
+    {
+        float maxspeed = 3.0f;
+        float speed = 0.1f;
+
+        public void AddVelocity(Vector2 dir)
+        {
+            dir += velocity;
+            if (velocity.X > maxspeed)
+                velocity.X = maxspeed;
+
+            else if (velocity.X < -maxspeed)
+                velocity.X = -maxspeed;
+                
+        }
+    }
+}
