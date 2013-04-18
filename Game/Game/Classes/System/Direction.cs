777109b5af5161ca2eb05501df4bbf43d5fe40c2
Despite <despite.mn@gmail.com>
2013-04-19 00:16:04 +0300

    Direction class

---
+++ Game/Game/Classes/System/Direction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public enum DIRECTION
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+}
