b7e7ad3336a23b2ca80911426dd4fdde48922d14
finparasite <finparasite@gmail.com>
2013-04-18 20:05:42 +0300

    Added classes

---
+++ Game/Game/Classes/Game/GameItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    class GameItem
+    {
+        protected string name;
+        protected string audioName;
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
