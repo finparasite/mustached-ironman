9ca44ce52b9355688d34d07f2cecdd19bc965d71
Despite <despite.mn@gmail.com>
2013-04-19 00:16:32 +0300

    InputManager Class

---
+++ Game/Game/Classes/System/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game
+{
+    static class InputManager
+    {
+        public static void Update()
+        {
+            KeyboardState ks = Keyboard.GetState();
+            Vector2 moveVector = Vector2.Zero;
+
+            if (ks.IsKeyDown(Keys.W))
+                moveVector += -Vector2.UnitY;
+            if (ks.IsKeyDown(Keys.D))
+                moveVector += -Vector2.UnitX;
+            if (ks.IsKeyDown(Keys.S))
+                moveVector += Vector2.UnitY;
+            if (ks.IsKeyDown(Keys.A))
+                moveVector += -Vector2.UnitX;
+
+            Level.Player.AddVelocity(moveVector.Normalize());
+
+            MouseState ms = Mouse.GetState();
+            if (ms.LeftButton == ButtonState.Pressed)
+                Level.Player.Fire(new Vector2(ms.X, ms.Y));
+        }
+    }
+}
